2b3e20652248d6fa82e1f532fe06b8bdb996e4ec
Salman Ahmed <say2salmanahmed@gmail.com>
2024-02-27 13:42:52 +0500

    Front-End Connected

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,69 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const App = () => {
 
-  axios.get('')
+  const [users, setUsers] = useState([]);
+
+  // UseRef
+  const name = useRef();
+  const email = useRef();
+
+  // UseEffect
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  
+  const getUsers = () => {
+    axios.get('http://localhost:3000/api/v1/users')
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((rej) => {
+        console.log(rej);
+      })
+  }
+
+  const addUser = (event) => {
+    event.preventDefault();
+
+    axios.post('http://localhost:3000/api/v1/users', {
+      name: name.current.value,
+      email: email.current.value
+    })
+      .then((res) => {
+        name.current.value = '';
+        email.current.value = '';
+        getUsers();
+
+      })
+      .catch((rej) => {
+        console.log(rej);
+      })
+  }
 
 
   return (
     <>
-      <h1>Hello!</h1>
+      <h1> Welcome! </h1>
+
+      <form onSubmit={addUser}>
+        <input type="text" ref={name} placeholder='Name' />
+        <input type="email" ref={email} placeholder='Email' />
+        <button type="submit"> Add User </button>
+      </form>
+
+      <div>
+        {users.length > 0 ? users.map((user) => {
+          return <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h3> {user.email} </h3>
+            <h4> {user.id} </h4>
+            <hr />
+          </div>
+        }) : <h3> Loading... </h3>}
+      </div>
     </>
   )
 }
